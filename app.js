8588107660e635f91f7d0d86bf4f1b5c95c56c96
Shawn Doherty <sdoherty@redhat.com>
2017-08-22 08:35:23 -0400

    change index to home and move folders

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.set("view engine", "ejs")
+
 app.get('/', function (req, res) {
-  res.render('index.ejs')
+  res.render("home")
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
