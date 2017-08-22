3745289d9db234026b93be1c153d93314ce9af13
Shawn Doherty <sdoherty@redhat.com>
2017-08-22 10:06:37 -0400

    change env variable for server listen

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/', function (req, res) {
   res.render("home.ejs")
 });
 
-app.listen(8080, function(){
+// app.listen(8080, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Countdown started")
 });
